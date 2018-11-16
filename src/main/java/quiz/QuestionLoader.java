f7d8f01499e7540df13c71e2f999e0e8b5884228
hm-sinning <hm-sinning@HM07000-L-EC-25.ads.mwn.de>
2018-11-16 16:38:42 +0100

    opencsv in pom eingefügt

---
+++ src/main/java/quiz/QuestionLoader.java
@@ -0,0 +1,11 @@
+package main.java.quiz;
+import java.io.FileReader;
+
+import com.opencsv.CSVReader;
+
+public class QuestionLoader {
+	
+	public QuestionLoader() {
+	//	CSVReader reader = new CSVReader(new FileReader("emps.csv"), ',');
+	}
+}
