f887fcd368a5a5112a1d560bef61e76721304017
fbsinn <https://github.com/fbsinn/AngwMathe.git>
2018-12-09 20:10:56 +0100

    Dateiendung .CSV in .csv umgewandelt und JavadocKommentare von Questionloader gelöscht

--- src/main/java/quiz/QuestionLoader.java
+++ src/main/java/quiz/QuestionLoader.java
@@ -11,13 +11,11 @@ import com.opencsv.bean.CsvBindByName;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-/** The QuestionLoader loads question that are needed in a quiz round.
- *  @author fabian sinning
- * */
+
+
 public class QuestionLoader {
-	/** This class saves every content from row of a csv file that is read by the CSV reader.
-	 *  @author fabian sinning
-	 */
+
+
 	public static class QuestionCSV {
 			@CsvBindByName(column = "")
 			public int index;
@@ -46,18 +44,12 @@ public class QuestionLoader {
 	
 	private Region region;
 	
-	/** Sets a new resource identifier for that QuestionLoader.
-	 *  @author fabian sinning
-	 *  @param region A resource identifier for the CSV reader
-	 *  @return Reference of that QuestionLoader
-	 * */
+
 	public QuestionLoader(Region r) {
 		this.region = r;
 	}
 	
-	/** Loads an Array of Question.
-	 *  @author fabian sinning
-	 * */
+
 	public void load() {
 		InputStreamReader input = new InputStreamReader(QuestionLoader.class.getResourceAsStream("/"+region.id+".csv"));
 		CsvToBean<QuestionCSV> csvToBean = new CsvToBeanBuilder<QuestionCSV>(input)
