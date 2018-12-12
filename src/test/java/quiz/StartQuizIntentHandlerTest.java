36cfc5cb41f2f538aaba4b39d271d96f05177680
fbsinn <https://github.com/fbsinn/AngwMathe.git>
2018-12-12 17:05:51 +0100

    neue pom dependency "alexa-skills-kit-tester-java" und StartQuizIntentHanlderTest.java

---
+++ src/test/java/quiz/StartQuizIntentHandlerTest.java
@@ -0,0 +1,23 @@
+package quiz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+
+import quiz.handlers.StartQuizIntentHandler;
+
+public class StartQuizIntentHandlerTest {
+
+	@Test
+	public void test() {
+	// alexa-skill-kit tester (siehe kay lerch)
+	// 
+	// noch nicht implementiert
+	//	StartQuizIntentHandler handler = new StartQuizIntentHandler();
+	//	handler.handle(HandlerInput.builder().build());
+	//	fail("Not yet implemented");
+	}
+
+}
